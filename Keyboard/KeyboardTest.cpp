15c157b414771abeff9570a1c3e556b59b719fd8
naweiss <nadavw2@gmail.com>
2017-07-06 16:54:25 +0300

    Keyboard Fixes

--- Keyboard/KeyboardTest.cpp
+++ Keyboard/KeyboardTest.cpp
@@ -2,10 +2,11 @@
 #include "Keyboard.cpp"
 #include <string>
 
-static int lvl_x;
 static int lvl_y;
+static int lvl_x;
 
 void setup(){
+	lvl_y = 0;
 	lvl_x = 0;
 	height /= 2;
 	width /= 2;
@@ -15,14 +16,14 @@ void draw(){
 	int key = KeyDown();
 	if (key != -1){
 		string str = to_string(key);
-		drawText(lvl_y*30,lvl_x*18,str.c_str(),str.length());
-		lvl_x++;
-	}
-	if (lvl_x*18 > height){
-		lvl_x = 0;
+		drawText(lvl_x*30,lvl_y*18,str.c_str(),str.length());
 		lvl_y++;
 	}
-	if (lvl_y*30 > width){
+	if ((lvl_y+1)*18 > height){
+		lvl_y = 0;
+		lvl_x++;
+	}
+	if ((lvl_x+1)*30 > width){
 		lvl_y = lvl_x = 0;
 		background();
 	}
